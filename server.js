203a256f30ce9f7a43af3a62dfa1e3e34117db33
¨guy¨ <guy@teamholodeck.com>
2016-08-10 19:08:56 +0000

    https:

--- server.js
+++ server.js
@@ -1,36 +1,56 @@
 //Load the request module
 var request = require('request');
+var lastTotalAvailable;
+var data = {};
 
+function checkCurrentCount(callback) {
 //Lets configure and request
 request({
-    url: 'https://ferrero.firebaseio.com/giveCount.json', //URL to hit
+    url: 'https://ferrero.firebaseio.com/nadiv1.json',
     method: 'GET', //Specify the method
-    //headers: { //We can define headers too
-      //  'Content-Type': 'MyContentType',
-        //'Custom-Header': 'Custom Value'
-  //  }
-}, function(error, response, body){
+ }, function(error, response, body){
     if(error) {
         console.log(error);
     } else {
         console.log(response.statusCode, body);
+	callback.call(this, body);
     }
 });
+};
+
+var interval = setInterval(function () {
+	checkCurrentCount(function (response) {
+		response=JSON.parse(response);
+		data.currentAvailable = response.currentAvailable;
+		data.message = response.message;
+		data.giveCount = response.giveCount;
+		data.totalAvailable = response.totalAvailable;
+console.log(data);
+
+		//if (lastTotalAvailable > 0 && lastGiveCount!=response.lastGiveCount)
+		//console.log('and the answer is...', response);
+
+		//lastGiveCount = response.lastGiveCount;
+	});
+}, 3000);
+
+
 
 //Lets configure and request
-/* request({
-    url: 'https://modulus.io/contact/demo', //URL to hit
-    qs: {from: 'blog example', time: +new Date()}, //Query string data
-    method: 'POST',
-    headers: {
-        'Content-Type': 'MyContentType',
-        'Custom-Header': 'Custom Value'
-    },
-    body: 'Hello Hello! String body!' //Set the body as a string
+function update(key, value){ 
+	var body = {};
+	body[key]=value;
+request({
+    url: 'https://ferrero.firebaseio.com/nadiv1.json', 
+   method: 'PATCH',
+     body: JSON.stringify(body) //Set the body as a string
 }, function(error, response, body){
     if(error) {
         console.log(error);
     } else {
         console.log(response.statusCode, body);
     }
-}); */
+}); 
+};
+
+update('message', 'key');
